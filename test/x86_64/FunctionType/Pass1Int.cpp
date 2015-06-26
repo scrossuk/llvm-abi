861effe007d92c52b6b04f1ebd5bb75372248cd1
Stephen Cross <scross@scross.co.uk>
2015-06-26 15:58:12 +0100

    Deleted all the old C++ tests, since they've now been replaced by LLVM IR type-parsing-based tests.

--- test/x86_64/FunctionType/Pass1Int.cpp
+++
@@ -1,8 +0,0 @@
-TEST(x86_64, Pass1Int) {
-	const auto functionType = getX86_64FnType(FunctionType(VoidTy, { IntTy }));
-	
-	EXPECT_FALSE(functionType->isVarArg());
-	EXPECT_TRUE(functionType->getReturnType()->isVoidTy());
-	ASSERT_EQ(functionType->getNumParams(), 1);
-	EXPECT_TRUE(functionType->getParamType(0)->isIntegerTy(32));
-}
--- test/x86_64/FunctionType/Pass2Ints.cpp
+++
@@ -1,9 +0,0 @@
-TEST(x86_64, Pass2Ints) {
-	const auto functionType = getX86_64FnType(FunctionType(VoidTy, { IntTy, IntTy }));
-	
-	EXPECT_FALSE(functionType->isVarArg());
-	EXPECT_TRUE(functionType->getReturnType()->isVoidTy());
-	ASSERT_EQ(functionType->getNumParams(), 2);
-	EXPECT_TRUE(functionType->getParamType(0)->isIntegerTy(32));
-	EXPECT_TRUE(functionType->getParamType(1)->isIntegerTy(32));
-}
