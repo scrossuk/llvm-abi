2516d66f938917edfafe91434d5390d4415d525e
Stephen Cross <scross@scross.co.uk>
2015-07-03 22:38:33 +0100

    Fixed InAlloca to only be enabled for LLVM 3.5+.

--- lib/x86/X86_64ABI.cpp
+++ lib/x86/X86_64ABI.cpp
@@ -152,7 +152,10 @@ namespace llvm_abi {
 			// Attach attributes to inalloca argument.
 			if (functionIRMapping.hasInallocaArg()) {
 				llvm::AttrBuilder attrs;
+#if LLVMABI_LLVM_VERSION >= 305
+				// InAlloca support was added in LLVM 3.5.
 				attrs.addAttribute(llvm::Attribute::InAlloca);
+#endif
 				attributes.push_back(llvm::AttributeSet::get(context(), functionIRMapping.inallocaArgIndex() + 1, attrs));
 			}
 			
